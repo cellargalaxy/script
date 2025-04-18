0d0d1358a523febf133d39d30e72ddc710d0420b
lixinzhao <lixinzhao@bytedance.com>
2025-04-18 15:14:13 +0800

    rtsp

--- rtsp/clean.sh
+++ rtsp/clean.sh
@@ -5,9 +5,9 @@ mkdir -p snapshots
 mkdir -p recordings
 
 while true; do
+    sleep 60
     echo 'clean start'
     ls -t snapshots/*.jpg | tail -n +$((5+1)) | xargs rm -f
     ls -t recordings/*.mp4 | tail -n +$((5+1)) | xargs rm -f
     echo 'clean done'
-    sleep 60
 done
