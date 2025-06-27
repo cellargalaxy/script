525af11d3fef19b95fc74587da6430ae1edd302b
cellargalaxy <cellargalaxy@gmail.com>
2025-06-27 16:53:32 +0000

    speaker-diarizatio

--- speaker-diarization/aaa/audio_batch.py
+++ speaker-diarization/aaa/audio_batch.py
@@ -2,6 +2,7 @@ import util
 import json
 import util_sub
 import os
+import math
 
 logger = util.get_logger()
 
@@ -9,22 +10,24 @@ logger = util.get_logger()
 def audio_batch(audio_activity_path, output_dir, min_silene_duration_ms=500, min_speech_duration_ms=1000 * 5):
     content = util.read_file(audio_activity_path)
     segments = json.loads(content)
-    segments = util_sub.gradual_segments(segments, gradual_duration_ms=min_silene_duration_ms)
-    # batches = []
-    # for i, segment in enumerate(segments):
-    #     if len(batches) == 0:
-    #         batches.append({"start": 0, "end": 0})
-    #         continue
-    #     start = batches[len(batches) - 1]['start']
-    #     end = segment['end']
-    #     duration = end - start
-    #     if min_speech_duration_ms <= duration and segment['vad_type'] == 'silene':
-    #         batches[len(batches) - 1]['end'] = end
-    #         batches.append({"start": end, "end": 0})
-    # batches.pop()
-    # batches[len(batches) - 1]['end'] = segments[len(segments) - 1]['end']
-    # util_sub.check_segments(batches)
-    # segments = batches
+    batches = []
+    for i, segment in enumerate(segments):
+        if len(batches) == 0:
+            batches.append({"start": 0, "end": 0})
+            continue
+        if segment['vad_type'] != 'silene':
+            continue
+        if segment['end'] - segment['start'] < min_silene_duration_ms:
+            continue
+        if segment['end'] - batches[len(batches) - 1]['start'] < min_speech_duration_ms:
+            continue
+        mean = math.floor((segments[i]['end'] + segments[i]['start']) / 2.0)
+        batches[len(batches) - 1]['end'] = mean
+        batches.append({"start": mean, "end": 0})
+    batches.pop()
+    batches[len(batches) - 1]['end'] = segments[len(segments) - 1]['end']
+    util_sub.check_segments(batches)
+    segments = batches
 
     audio_batch_path = os.path.join(output_dir, 'audio_batch.json')
     util.save_file(json.dumps(segments), audio_batch_path)
