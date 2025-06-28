a9f07edbcf435836a6c7f68506a1bf0bad35dd29
cellargalaxy <cellargalaxy@gmail.com>
2025-06-28 16:22:15 +0000

    speaker-diarizatio

--- speaker-diarization/aaa/gen_subt_whisperx.py
+++ speaker-diarization/aaa/gen_subt_whisperx.py
@@ -13,14 +13,20 @@ def gen_subt(audio_path, auth_token):
     model = whisperx.load_model("large-v3", device, compute_type=compute_type)
     audio = whisperx.load_audio(audio_path)
     result = model.transcribe(audio, batch_size=16)
+    del model
+    util.exec_gc()
 
     model_a, metadata = whisperx.load_align_model(language_code=result["language"], device=device)
     aligned_result = whisperx.align(result["segments"], model_a, metadata, audio, device, return_char_alignments=False)
     aligned_result["language"] = result["language"]
+    del model_a
+    util.exec_gc()
 
     diarize_model = whisperx.diarize.DiarizationPipeline(use_auth_token=auth_token, device=device)
     diarize_segments = diarize_model(audio)
     diarize_result = whisperx.assign_word_speakers(diarize_segments, aligned_result)
     diarize_result["language"] = result["language"]
+    del diarize_model
+    util.exec_gc()
 
     return diarize_result
