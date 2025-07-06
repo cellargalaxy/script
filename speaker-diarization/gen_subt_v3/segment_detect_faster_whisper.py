abf8c9d2dd492c7606444132df5c25af18c32a43
cellargalaxy <cellargalaxy@gmail.com>
2025-07-06 09:35:06 +0000

    speaker-diarizatio

--- speaker-diarization/gen_subt_v3/segment_detect_faster_whisper.py
+++ speaker-diarization/gen_subt_v3/segment_detect_faster_whisper.py
@@ -1,5 +1,7 @@
 from faster_whisper import WhisperModel
 import util
+import util_subt
+from pydub import AudioSegment
 
 logger = util.get_logger()
 
@@ -23,6 +25,9 @@ def exec_gc():
 def segment_detect(audio_path, auth_token):
     logger.info("字幕生成: %s", audio_path)
 
+    audio = AudioSegment.from_wav(audio_path)
+    last_end = len(audio)
+
     model = get_model()
     segments, info = model.transcribe(audio_path, beam_size=5)
     subt = {
@@ -36,5 +41,33 @@ def segment_detect(audio_path, auth_token):
             "text": segment.text,
         }
         subt['segments'].append(obj)
+
+    for i, segment in enumerate(subt['segments']):
+        start = round(segment['start'] * 1000)
+        if last_end <= start:
+            continue
+        end = round(segment['end'] * 1000)
+        if last_end <= end:
+            segment['end'] = last_end / 1000.0
+        if segment['end'] < segment['start']:
+            continue
+        segments.append(segment)
+    subt['segments'] = segments
+
+    segments = []
+    for i, segment in enumerate(subt['segments']):
+        if i == 0:
+            segments.append(subt['segments'][i])
+            continue
+        if subt['segments'][i]['start'] < subt['segments'][i - 1]['end']:
+            continue
+        segments.append(subt['segments'][i])
+    subt['segments'] = segments
+
+    util_subt.check_discrete_segments(subt['segments'])
+
+    del model
+    del segments
+    util.exec_gc()
 
     return subt
