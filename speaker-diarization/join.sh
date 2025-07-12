534c1f696e94a0e83b0144e4d2b006e5082aa27b
cellargalaxy <cellargalaxy@gmail.com>
2025-07-12 11:47:57 +0800

    speaker-diarization

--- speaker-diarization/join.sh
+++ speaker-diarization/join.sh
@@ -8,7 +8,12 @@ ffmpeg -i aaaaa/demo.mkv -i aaaaa/output/demo/demucs/htdemucs/wav/vocals.wav
 output_video.mkv
 
 
-
+gen_subt_v1/bin/ffmpeg -i long.mkv -y -map 0 -map -0:a -map 0:a:0 -c copy long_jpn.mkv
+gen_subt_v1/bin/ffmpeg -i long.mkv -y -map 0 -map -0:a -map 0:a:1 -c copy long_eng.mkv
+gen_subt_v1/bin/ffmpeg -i long.mkv -y -map 0:a:0 -vn -ar 16000 long_jpn.wav
+gen_subt_v1/bin/ffmpeg -i long.mkv -y -map 0:a:1 -vn -ar 16000 long_eng.wav
+gen_subt_v1/bin/ffmpeg -i long.mkv -y -map 0:a:0 -vn -ar 16000 -ac 1 long_jpn_single.wav
+gen_subt_v1/bin/ffmpeg -i long.mkv -y -map 0:a:1 -vn -ar 16000 -ac 1 long_eng_single.wav
 
 
 bin/ffmpeg -i '../S01E01.mkv' -y -vf \
@@ -21,7 +26,14 @@ ffmpeg -i long.mkv -y -vf \
 "drawtext=text='%{pts\:hms}':fontsize=96:fontcolor=white:x=10:y=10, \
 drawtext=text='Seconds\: %{pts}':fontsize=96:fontcolor=white:x=10:y=100, \
 drawtext=text='Frame\: %{n}':fontsize=96:fontcolor=white:x=10:y=200" \
--t 00:06:45 -map 0 -c:a copy test.mkv
+-t 00:06:45 -map 0 -c:a copy demo.mkv
+
+gen_subt_v1/bin/ffmpeg -i demo.mkv -y -map 0 -map -0:a -map 0:a:0 -c copy demo_jpn.mkv
+gen_subt_v1/bin/ffmpeg -i demo.mkv -y -map 0 -map -0:a -map 0:a:1 -c copy demo_eng.mkv
+gen_subt_v1/bin/ffmpeg -i demo.mkv -y -map 0:a:0 -vn -ar 16000 demo_jpn.wav
+gen_subt_v1/bin/ffmpeg -i demo.mkv -y -map 0:a:1 -vn -ar 16000 demo_eng.wav
+gen_subt_v1/bin/ffmpeg -i demo.mkv -y -map 0:a:0 -vn -ar 16000 -ac 1 demo_jpn_single.wav
+gen_subt_v1/bin/ffmpeg -i demo.mkv -y -map 0:a:1 -vn -ar 16000 -ac 1 demo_eng_single.wav
 
 bin/ffmpeg -ss 0 -to 4.785343750000001 -i output/demo/demucs/mkv.mkv -c copy -y 00000_silene.mkv
 bin/ffmpeg -ss 4.78534375000000 -to 112.59159375 -i output/demo/demucs/mkv.mkv -c copy -y 00001_speech.mkv
