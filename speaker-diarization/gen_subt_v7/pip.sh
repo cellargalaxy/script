7e638662d5a1f33b201881df93bf2874c4b04b62
cellargalaxy <cellargalaxy@gmail.com>
2025-10-18 13:50:53 +0000

    speaker-diarization

--- speaker-diarization/gen_subt_v7/pip.sh
+++ speaker-diarization/gen_subt_v7/pip.sh
@@ -1,14 +1,13 @@
 pip install inputimeout
 pip install --upgrade torch==2.6.0+cu126 torchvision==0.21.0+cu126 torchaudio==2.6.0+cu126 --index-url https://download.pytorch.org/whl/cu126
 pip install "audio-separator[gpu]"
+sudo apt install -y libc++1 && pip install -U --force-reinstall -v git+https://github.com/TEN-framework/ten-vad.git
 
 
 
-sudo apt install -y libc++1 # ten-vad需安装
 pip install numpy
 pip install pysubs2
 pip install pydub
-pip install -U --force-reinstall -v git+https://github.com/TEN-framework/ten-vad.git
 pip install pyannote.audio==3.4.0
 pip install faster-whisper
 pip install pyloudnorm
