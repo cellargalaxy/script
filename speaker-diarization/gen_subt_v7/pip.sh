c94b23639b14acd241f1e0d616db69f2ae5cac7e
cellargalaxy <cellargalaxy@gmail.com>
2025-10-18 09:00:10 +0000

    speaker-diarization

---
+++ speaker-diarization/gen_subt_v7/pip.sh
@@ -0,0 +1,1 @@
+pip install --upgrade torch==2.6.0+cu126 torchvision==0.21.0+cu126 torchaudio==2.6.0+cu126 --index-url https://download.pytorch.org/whl/cu126
---
+++ speaker-diarization/gen_subt_v7/util_ffmpeg.py
@@ -0,0 +1,38 @@
+import util
+
+logger = util.get_logger()
+
+
+def extract_audio_track(video_path, index, audio_path):
+    util.mkdir(audio_path)
+    cmd = [
+        'ffmpeg',
+        '-i', video_path,
+        '-map', "0:a:{}".format(index),  # 在第0个输入文件中选择第index个音轨
+        '-ar', '44100',
+        '-ac', '2',
+        '-y',
+        audio_path
+    ]
+    logger.info("提取视频文件音轨,cmd: %s", util.json_dumps(cmd))
+    return_code = util.popen_cmd(cmd)
+    if return_code != 0:
+        logger.error("提取视频文件音轨，异常")
+        raise ValueError("提取视频文件音轨，异常")
+
+
+def merge_audio_channel(input_path, output_path):
+    util.mkdir(output_path)
+    cmd = [
+        'ffmpeg',
+        '-i', input_path,
+        '-ac', '1',
+        '-ar', '16000',
+        '-y',
+        output_path
+    ]
+    logger.info("合并音频声道,cmd: %s", util.json_dumps(cmd))
+    return_code = util.popen_cmd(cmd)
+    if return_code != 0:
+        logger.error("合并音频声道，异常")
+        raise ValueError("合并音频声道，异常")
