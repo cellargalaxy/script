02bb42d38b997402eb7c908b656a7a3e8ca69b20
cellargalaxy <cellargalaxy@gmail.com>
2025-11-14 17:11:12 +0000

    speaker-diarization

--- speaker-diarization/gen_subt_v7/pip.sh
+++ speaker-diarization/gen_subt_v7/pip.sh
@@ -1,5 +1,5 @@
 pip install inputimeout
-pip install --upgrade torch==2.6.0+cu126 torchvision==0.21.0+cu126 torchaudio==2.6.0+cu126 --index-url https://download.pytorch.org/whl/cu126
+pip install --upgrade torch==2.8.0+cu126 torchvision==0.23.0+cu126 torchaudio==2.8.0+cu126 --index-url https://download.pytorch.org/whl/cu126
 pip install "audio-separator[gpu]"
 pip install pyloudnorm
 sudo apt install -y libc++1 && pip install -U --force-reinstall -v git+https://github.com/TEN-framework/ten-vad.git
