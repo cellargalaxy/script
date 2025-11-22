579d8730095b73f40ff684782c6b704a5ea53b44
cellargalaxy <cellargalaxy@gmail.com>
2025-11-22 08:26:33 +0000

    speaker-diarization

--- speaker-diarization/gen_subt_v7/extract_stem_uvr.py
+++ speaker-diarization/gen_subt_v7/extract_stem_uvr.py
@@ -6,6 +6,11 @@ model_file_dir = os.path.join(util.get_home_dir(), '.cache', 'uvr')
 
 
 class VocalHandler:
+    def __init__(self, model_name=None):
+        if not model_name:
+            model_name = 'vocals_mel_band_roformer.ckpt'
+        self.model_name = model_name
+
     def get_name(self):
         return "vocal"
 
@@ -16,17 +21,17 @@ class VocalHandler:
         return "bgm.wav"
 
     def extract(self, audio_path, output_dir):
-        vocals_path, others_path = extract_vocal(audio_path, output_dir)
+        vocals_path, others_path = extract_vocal(self.model_name, audio_path, output_dir)
         return vocals_path, others_path
 
 
-def extract_vocal(audio_path, output_dir):
+def extract_vocal(model_name, audio_path, output_dir):
     vocals_path = os.path.join(output_dir, 'vocals.wav')
     others_path = os.path.join(output_dir, 'others.wav')
     if util.path_exist(vocals_path) and util.path_exist(others_path):
         return vocals_path, others_path
     separator = Separator(model_file_dir=model_file_dir, output_dir=output_dir)
-    separator.load_model(model_filename='vocals_mel_band_roformer.ckpt')
+    separator.load_model(model_filename=model_name)
     output_names = {
         "Vocals": "vocals",
         "Other": "others",
@@ -36,6 +41,11 @@ def extract_vocal(audio_path, output_dir):
 
 
 class MainVocalHandler:
+    def __init__(self, model_name=None):
+        if not model_name:
+            model_name = 'mel_band_roformer_karaoke_becruily.ckpt'
+        self.model_name = model_name
+
     def get_name(self):
         return "main_vocal"
 
@@ -46,17 +56,17 @@ class MainVocalHandler:
         return "harmony.wav"
 
     def extract(self, audio_path, output_dir):
-        vocals_path, instrumental_path = extract_main_vocal(audio_path, output_dir)
+        vocals_path, instrumental_path = extract_main_vocal(self.model_name, audio_path, output_dir)
         return vocals_path, instrumental_path
 
 
-def extract_main_vocal(audio_path, output_dir):
+def extract_main_vocal(model_name, audio_path, output_dir):
     vocals_path = os.path.join(output_dir, 'vocals.wav')
     instrumental_path = os.path.join(output_dir, 'instrumental.wav')
     if util.path_exist(vocals_path) and util.path_exist(instrumental_path):
         return vocals_path, instrumental_path
     separator = Separator(model_file_dir=model_file_dir, output_dir=output_dir)
-    separator.load_model(model_filename='mel_band_roformer_karaoke_becruily.ckpt')
+    separator.load_model(model_filename=model_name)
     output_names = {
         "Vocals": "vocals",
         "Instrumental": "instrumental",
@@ -66,6 +76,11 @@ def extract_main_vocal(audio_path, output_dir):
 
 
 class DeReverbHandler:
+    def __init__(self, model_name=None):
+        if not model_name:
+            model_name = 'dereverb_mel_band_roformer_mono_anvuew.ckpt'
+        self.model_name = model_name
+
     def get_name(self):
         return "dereverb"
 
@@ -76,18 +91,17 @@ class DeReverbHandler:
         return "reverb.wav"
 
     def extract(self, audio_path, output_dir):
-        noreverb_path, reverb_path = extract_dereverb(audio_path, output_dir)
+        noreverb_path, reverb_path = extract_dereverb(self.model_name, audio_path, output_dir)
         return noreverb_path, reverb_path
 
 
-def extract_dereverb(audio_path, output_dir):
+def extract_dereverb(model_name, audio_path, output_dir):
     noreverb_path = os.path.join(output_dir, 'noreverb.wav')
     reverb_path = os.path.join(output_dir, 'reverb.wav')
     if util.path_exist(noreverb_path) and util.path_exist(reverb_path):
         return noreverb_path, reverb_path
     separator = Separator(model_file_dir=model_file_dir, output_dir=output_dir)
-    separator.load_model(model_filename='dereverb_mel_band_roformer_mono_anvuew.ckpt')
-    # separator.load_model(model_filename='MDX23C-De-Reverb-aufr33-jarredou.ckpt')
+    separator.load_model(model_filename=model_name)
     output_names = {
         "Noreverb": "noreverb",
         "Reverb": "reverb",
