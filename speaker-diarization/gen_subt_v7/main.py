31f80d8706e23a36309f7462a26bc9e833399870
cellargalaxy <cellargalaxy@gmail.com>
2025-11-22 08:27:03 +0000

    speaker-diarization

--- speaker-diarization/gen_subt_v7/main.py
+++ speaker-diarization/gen_subt_v7/main.py
@@ -24,8 +24,9 @@ def exec(manager):
     extract_stem.exec(manager, [
         extract_stem_uvr.VocalHandler(),
         extract_stem_uvr.MainVocalHandler(),
-        extract_stem_uvr.DeReverbHandler(),
+        extract_stem_uvr.DeReverbHandler('MDX23C-De-Reverb-aufr33-jarredou.ckpt'),
     ])
+    return
 
     import extract_loudness
     extract_loudness.exec(manager)
@@ -67,7 +68,7 @@ def exec_batch(video_paths):
 
 
 video_paths = [
-    '../material/panty.mkv',
+    # '../material/panty.mkv',
     # '../material/panty/[Nekomoe kissaten&LoliHouse] New PANTY & STOCKING with GARTERBELT - 01 [WebRip 1080p HEVC-10bit AACx2 ASSx2].mkv',
     # '../material/panty/[Nekomoe kissaten&LoliHouse] New PANTY & STOCKING with GARTERBELT - 02 [WebRip 1080p HEVC-10bit AACx2 ASSx2].mkv',
     # '../material/panty/[Nekomoe kissaten&LoliHouse] New PANTY & STOCKING with GARTERBELT - 03 [WebRip 1080p HEVC-10bit AACx2 ASSx2].mkv',
@@ -78,8 +79,11 @@ video_paths = [
     # '../material/panty/[Nekomoe kissaten&LoliHouse] New PANTY & STOCKING with GARTERBELT - 08 [WebRip 1080p HEVC-10bit AACx2 ASSx2].mkv',
     # '../material/panty/[Nekomoe kissaten&LoliHouse] New PANTY & STOCKING with GARTERBELT - 09 [WebRip 1080p HEVC-10bit AACx2 ASSx2].mkv',
     # '../material/panty/[Nekomoe kissaten&LoliHouse] New PANTY & STOCKING with GARTERBELT - 10 [WebRip 1080p HEVC-10bit AACx2 ASSx2].mkv',
+    '../material/my_way.webm',
+    # '../material/mao.mp3',
     # '../material/demo.mkv',
     # '../material/mkv.mkv',
+    # '../material/holo.mkv',
     # '../material/youtube-live/【狼と香辛料】「賢狼ホロのわっちチャンネル」 #1　あの賢狼ホロがYouTuberデビュー⁉ [Vup1WxjRaUY].mkv',
     # '../material/youtube-live/【狼と香辛料】「賢狼ホロのわっちチャンネル」 #2　「写真で一言」でまさかの珍回答⁉ [A1LoxukWcrs].mkv',
     # '../material/youtube-live/【狼と香辛料】「賢狼ホロのわっちチャンネル」 #3 「音あてクイズ」で賢狼の本領発揮⁉ [1LbrvnMbvKY].mkv',
