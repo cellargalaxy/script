7002cda43151a3087be59b4446931ce5f64e2f56
cellargalaxy <cellargalaxy@gmail.com>
2025-12-13 15:27:31 +0000

    speaker-diarization

--- speaker-diarization/gen_subt_v7/pip.sh
+++ speaker-diarization/gen_subt_v7/pip.sh
@@ -1,3 +1,5 @@
+# https://pytorch.org/get-started/previous-versions/
+
 conda env list
 conda remove -n gen_subt_v7 --all
 conda create -n gen_subt_v7 python=3.10
@@ -14,7 +16,18 @@ pip install pysubs2
 pip install faster-whisper
 pip install pyannote.audio==3.4.0
 pip install speechbrain
-pip install whisperx
+pip install whisperx #whisperx依赖torch==2.8.0，没法对torch降级
+
+
+conda env list
+conda remove -n audiosr --all
+conda create -n audiosr python=3.10
+conda activate audiosr
+conda deactivate
+
+pip install --upgrade torch==2.6.0+cu126 torchvision==0.21.0+cu126 torchaudio==2.6.0+cu126 --index-url https://download.pytorch.org/whl/cu126
+pip install audiosr
+
 
 pip freeze > requirements.txt
 pip install -r requirements.txt
