ae2688ddc1fc62e7ac220f56f0363281113d476f
lixinzhao <lixinzhao@bytedance.com>
2025-12-24 20:55:35 +0800

    speaker-diarization

---
+++ speaker-diarization/gen_subt_v8/part_detect_vad.py
@@ -0,0 +1,115 @@
+from pydub import AudioSegment
+import util
+import tool_ten_vad
+import math
+import tool_subt
+import tool_loudness
+import tool_nemo_vad
+
+logger = util.get_logger()
+
+
+def diffusion(tags, tag):
+    for i, _ in enumerate(tags):
+        if i == 0:
+            continue
+        if tags[i - 1] == tag and tags[i] == 0:
+            tags[i] = tag
+    for i in range(len(tags) - 2, -1, -1):
+        if tags[i] == 0 and tags[i + 1] == tag:
+            tags[i] = tag
+    return tags
+
+
+def part_detect_by_data(audio,
+                        frame_rate=50,
+                        speech_threshold=0.8,
+                        silence_threshold=0.2,
+                        volume_threshold=-80,
+                        ):
+    last_end = len(audio)
+
+    volumes = tool_loudness.get_loudness(audio, frame_rate)
+    if len(volumes) == 0:
+        logger.error(f"响度为空")
+        raise ValueError(f"响度为空")
+    ten_vads = tool_ten_vad.vad_confidence(audio, frame_rate)
+    if len(ten_vads) == 0:
+        logger.error(f"人声置信度为空")
+        raise ValueError(f"人声置信度为空")
+    nemo_vads = tool_nemo_vad.vad_confidence(audio)
+    if len(nemo_vads) == 0:
+        logger.error(f"人声置信度为空")
+        raise ValueError(f"人声置信度为空")
+    if not len(volumes) == len(ten_vads) == len(nemo_vads):
+        logger.error(
+            f"人声置信度与响度长度不一致, volumes: {len(volumes)}, ten_vads: {len(ten_vads)}, nemo_vads: {len(nemo_vads)}")
+        raise ValueError(
+            f"人声置信度与响度长度不一致, volumes: {len(volumes)}, ten_vads: {len(ten_vads)}, nemo_vads: {len(nemo_vads)}")
+
+    tags = [0] * len(ten_vads)
+    for i, volume in enumerate(volumes):
+        if tags[i] == 0 and volumes[i] <= volume_threshold:
+            tags[i] = -1
+    for i, vad in enumerate(ten_vads):
+        if tags[i] == 0 and vad <= silence_threshold:
+            tags[i] = -1
+        elif tags[i] == 0 and speech_threshold <= vad:
+            tags[i] = 1
+    for i, vad in enumerate(nemo_vads):
+        if tags[i] == 0 and vad <= silence_threshold:
+            tags[i] = -1
+        elif tags[i] == 0 and speech_threshold <= vad:
+            tags[i] = 1
+
+    tags = diffusion(tags, 1)
+    tags = diffusion(tags, -1)
+    for i, tag in enumerate(tags):
+        if tags[i] == 0:
+            tags[i] = 1
+
+    segments = []
+    for i, tag in enumerate(tags):
+        if tag == 1:
+            vad_type = 'speech'
+        elif tag == -1:
+            vad_type = 'silence'
+        else:
+            logger.error(f"非法人声标签: {tag}")
+            raise ValueError(f"非法人声标签: {tag}")
+        if i > 0 and tags[i - 1] != tags[i]:
+            pre_end = segments[-1]['end']
+            segments.append({'start': pre_end, 'end': 0, 'vad_type': vad_type})
+        if len(segments) == 0:
+            segments.append({'start': 0, 'end': 0, 'vad_type': vad_type})
+        end = math.floor((i + 1) * (1000.0 / frame_rate))
+        segments[-1]['end'] = end
+
+    if last_end < segments[-1]['end']:
+        segments[-1]['end'] = last_end
+    if segments[-1]['end'] < last_end:
+        pre_end = segments[-1]['end']
+        segments.append({'start': pre_end, 'end': last_end, 'vad_type': 'silence'})
+
+    segments = tool_subt.fix_overlap_segments(segments)
+    segments = tool_subt.unit_segments(segments, 'vad_type')
+    segments = tool_subt.init_segments(segments)
+    tool_subt.check_coherent_segments(segments)
+
+    return segments
+
+
+def part_detect(audio_path,
+                frame_rate=50,
+                speech_threshold=0.8,
+                silence_threshold=0.2,
+                volume_threshold=-80,
+                ):
+    audio = AudioSegment.from_wav(audio_path)
+    segments = part_detect_by_data(audio,
+                                   frame_rate=frame_rate,
+                                   speech_threshold=speech_threshold,
+                                   silence_threshold=silence_threshold,
+                                   volume_threshold=volume_threshold,
+                                   )
+    return segments
---
+++ speaker-diarization/gen_subt_v8/tool_subt.py
@@ -0,0 +1,180 @@
+import util
+import pysubs2
+import math
+
+logger = util.get_logger()
+
+
+def init_segments(segments):
+    for i, segment in enumerate(segments):
+        segments[i]['index'] = i
+        segments[i]['duration'] = segments[i]['end'] - segments[i]['start']
+    return segments
+
+
+def fix_overlap_segments(segments):
+    for i, segment in enumerate(segments):
+        if i == 0:
+            continue
+        if segments[i - 1]['end'] <= segments[i]['start']:
+            continue
+        mean = math.floor((segments[i - 1]['end'] + segments[i]['start']) / 2.0)
+        segments[i]['start'] = mean
+        segments[i - 1]['end'] = mean
+    return segments
+
+
+def unit_segments(segments, type_key, type_value=None):
+    results = []
+    for i, segment in enumerate(segments):
+        if type_value and segment[type_key] != type_value:
+            results.append(segment)
+            continue
+        pre_type = ''
+        if len(results) > 0:
+            pre_type = results[-1].get(type_key, '')
+        if segment.get(type_key,None) == pre_type:
+            results[-1]['end'] = segment['end']
+        else:
+            results.append(segment)
+    return results
+
+
+def fill_segments(segments, last_end=None, vad_type=None):
+    full = []
+    for i, segment in enumerate(segments):
+        pre_end = 0
+        if i > 0:
+            pre_end = segments[i - 1]['end']
+        if pre_end < segments[i]['start']:
+            obj = {"start": pre_end, "end": segments[i]['start']}
+            if vad_type:
+                obj['vad_type'] = vad_type
+            full.append(obj)
+        full.append(segments[i])
+    if last_end and len(full) > 0 and full[-1]['end'] < last_end:
+        obj = {"start": full[-1]['end'], "end": last_end}
+        if vad_type:
+            obj['vad_type'] = vad_type
+        full.append(obj)
+    return full
+
+
+def clipp_segments(segments, last_end):
+    clipp = []
+    for i, segment in enumerate(segments):
+        if last_end < segment['start']:
+            continue
+        if last_end < segment['end']:
+            segment['end'] = last_end
+        clipp.append(segment)
+    return clipp
+
+
+def check_coherent_segments(segments):
+    """连贯"""
+    for i, segment in enumerate(segments):
+        start = segments[i].get('start', -1)
+        if not isinstance(start, int):
+            logger.error("检查segments，start类型非法: %s, segment:%s, %s", i,
+                         util.json_dumps(segment),
+                         util.json_dumps(segments))
+            raise ValueError("检查segments，start类型非法")
+        if start < 0:
+            logger.error("检查segments，start非法: %s, segment:%s, %s", i,
+                         util.json_dumps(segment),
+                         util.json_dumps(segments))
+            raise ValueError("检查segments，start非法")
+        end = segments[i].get('end', -1)
+        if not isinstance(end, int):
+            logger.error("检查segments，end类型非法: %s, segment:%s, %s", i,
+                         util.json_dumps(segment),
+                         util.json_dumps(segments))
+            raise ValueError("检查segments，end类型非法")
+        if end < 0:
+            logger.error("检查segments，end非法: %s, segment:%s, %s", i,
+                         util.json_dumps(segment),
+                         util.json_dumps(segments))
+            raise ValueError("检查segments，end非法")
+        if end <= start:
+            logger.error("检查segments，start与end非法: %s, segment:%s, %s", i,
+                         util.json_dumps(segment),
+                         util.json_dumps(segments))
+            raise ValueError("检查segments，start与end非法")
+        if i > 0:
+            pre_end = segments[i - 1]['end']
+            if pre_end != start:
+                logger.error("检查segments，pre_end与start非法: %s, segment:%s, %s", i,
+                             util.json_dumps(segment),
+                             util.json_dumps(segments))
+                raise ValueError("检查segments，pre_end与start非法")
+
+
+def check_discrete_segments(segments):
+    """离散"""
+    for i, segment in enumerate(segments):
+        start = segments[i].get('start', -1)
+        if not isinstance(start, int) and not isinstance(start, float):
+            logger.error("检查segments，start类型非法: %s, segment:%s, %s", i,
+                         util.json_dumps(segment),
+                         util.json_dumps(segments))
+            raise ValueError("检查segments，start类型非法")
+        if start < 0:
+            logger.error("检查segments，start非法: %s, segment:%s, %s", i,
+                         util.json_dumps(segment),
+                         util.json_dumps(segments))
+            raise ValueError("检查segments，start非法")
+        end = segments[i].get('end', -1)
+        if not isinstance(end, int) and not isinstance(end, float):
+            logger.error("检查segments，end类型非法: %s, segment:%s, %s", i,
+                         util.json_dumps(segment),
+                         util.json_dumps(segments))
+            raise ValueError("检查segments，end类型非法")
+        if end < 0:
+            logger.error("检查segments，end非法: %s, segment:%s, %s", i,
+                         util.json_dumps(segment),
+                         util.json_dumps(segments))
+            raise ValueError("检查segments，end非法")
+        if end <= start:
+            logger.error("检查segments，start与end非法: %s, segment:%s, %s", i,
+                         util.json_dumps(segment),
+                         util.json_dumps(segments))
+            raise ValueError("检查segments，start与end非法")
+        if i > 0:
+            pre_end = segments[i - 1]['end']
+            if start < pre_end:
+                logger.error("检查segments，pre_end与start非法: %s, segment:%s, %s", i,
+                             util.json_dumps(segment),
+                             util.json_dumps(segments))
+                raise ValueError("检查segments，pre_end与start非法")
+
+
+def shift_segments_time(segments, duration_ms):
+    for i, segment in enumerate(segments):
+        segments[i]['start'] = segments[i]['start'] + duration_ms
+        segments[i]['end'] = segments[i]['end'] + duration_ms
+    return segments
+
+
+def save_segments_as_srt(segments, save_path, skip_silence=False):
+    results = []
+    for i, segment in enumerate(segments):
+        index = i
+        if segment.get('index', 0):
+            index = segment.get('index', 0)
+        start = segment['start'] / 1000.0
+        end = segment['end'] / 1000.0
+        text = f"{index:04d} {segment['start']}>{segment['end']}"
+        if segment.get('vad_type', ''):
+            text = f"{text} {segment.get('vad_type', '')}"
+        if segment.get('speaker', ''):
+            text = f"{text} {segment.get('speaker', '')}"
+        if segment.get('text', ''):
+            text = f"{text} {segment.get('text', '')}"
+        if skip_silence and segment.get('vad_type', '') == 'silence':
+            continue
+        obj = {'start': start, 'end': end, 'text': text}
+        results.append(obj)
+    util.mkdir(save_path)
+    subs = pysubs2.load_from_whisper(results)
+    subs.save(save_path)
